145485903666ffeb5bdda8515f4a8304022411f9
Sumit-Meena1 <sumitmeena6944@gmail.com>
2025-12-09 20:44:56 +0530

    TipCalculator

---
+++ TipCalculator/script.js
@@ -0,0 +1,23 @@
+function calculate()
+{
+    let bill = Number(document.getElementById("billAmount").value);
+    let service = parseFloat(document.getElementById("service").value) / 100;
+    let people = Number(document.getElementById("people").value);
+
+    let billEach = bill/people;
+    let tipEach = (bill*service)/people;
+    let total = billEach+tipEach;
+
+    if(!people || !bill || !service)
+    {
+        document.getElementById("result").innerText = "Please Enter Correct Data";
+    }
+    else
+    {
+        document.getElementById("result").innerText = `Bill Amount: ${billEach} \n Tip: ${tipEach} \n Total Bill: ${total}`;
+    }
+
+    
+    
+    
+}
