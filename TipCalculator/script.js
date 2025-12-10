04e91901015a8d6e37bdc4204370be80090c857e
Sumit-Meena1 <sumitmeena6944@gmail.com>
2025-12-10 09:02:37 +0530

    TipCalculator improved

--- TipCalculator/script.js
+++ TipCalculator/script.js
@@ -1,23 +1,17 @@
-function calculate()
-{
-    let bill = Number(document.getElementById("billAmount").value);
-    let service = parseFloat(document.getElementById("service").value) / 100;
-    let people = Number(document.getElementById("people").value);
+function calculate() {
+  let bill = Number(document.getElementById("billAmount").value);
+  let service = parseFloat(document.getElementById("service").value) / 100;
+  let people = Number(document.getElementById("people").value);
 
-    let billEach = bill/people;
-    let tipEach = (bill*service)/people;
-    let total = billEach+tipEach;
+  let billEach = bill / people;
+  let tipEach = (bill * service) / people;
+  let total = billEach + tipEach;
 
-    if(!people || !bill || !service)
-    {
-        document.getElementById("result").innerText = "Please Enter Correct Data";
-    }
-    else
-    {
-        document.getElementById("result").innerText = `Bill Amount: ${billEach} \n Tip: ${tipEach} \n Total Bill: ${total}`;
-    }
-
-    
-    
-    
+  if (!people || !bill || !service) {
+    document.getElementById("result").innerText = "Please Enter Correct Data";
+  } else {
+    document.getElementById(
+      "result"
+    ).innerText = `Bill Amount: ${billEach}$ \n Tip: ${tipEach}$ \n Total Bill: ${total}$ \n For Each`;
+  }
 }
