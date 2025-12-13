6553987a48c21a811aa56adf5d673c212853dfd2
Sumit-Meena1 <sumitmeena6944@gmail.com>
2025-12-13 18:04:00 +0530

    Guessthenumber

--- Guessthenumber/script.js
+++ Guessthenumber/script.js
@@ -1,9 +1,11 @@
 let num = Math.floor(Math.random() * 10) + 1;
+let nom = Number(1);
 
 function submit() {
   let btn = document.getElementById("btn");
   let guess = Number(document.getElementById("guess").value);
   let icon = document.getElementById("icon");
+  
 
   if (btn.innerText === "Re-Start") {
     num = Math.floor(Math.random() * 10) + 1;
@@ -19,15 +21,18 @@ function submit() {
     return;
   }
   if (guess === num) {
-    document.getElementById("result").innerText =
-      "Congratulations! You Get It Right";
+    document.getElementById(
+      "result"
+    ).innerText = ` Congratulations! You Get It Right \n in ${nom} guesses `;
     icon.innerHTML = `<i class="bi bi-emoji-smile-fill" style="color: green;"></i>`;
     btn.innerText = "Re-Start";
   } else if (guess < num) {
     document.getElementById("result").innerText = "Oops! Try a larger number";
     icon.innerHTML = `<i class="bi bi-emoji-frown-fill" style="color: red;"></i>`;
+    nom = Number(nom+1);
   } else {
     document.getElementById("result").innerText = "Oops! Try a smaller number";
     icon.innerHTML = `<i class="bi bi-emoji-frown-fill" style="color: red;"></i>`;
+    nom = Number(nom+1);
   }
 }
