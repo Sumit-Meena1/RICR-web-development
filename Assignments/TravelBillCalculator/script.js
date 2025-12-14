114f487fc1e8501ca7a5d792e37fc83050d76854
Sumit-Meena1 <sumitmeena6944@gmail.com>
2025-12-14 15:06:05 +0530

    TravelBillCalculator

---
+++ Assignments/TravelBillCalculator/script.js
@@ -0,0 +1,50 @@
+// Double total ;
+// 		if(distance <=10)
+// 		{
+// 			total = distance *11;
+// 		}
+// 		else if(distance<=100)
+// 		{
+// 			total = (10*11)+((distance-10)*10);
+// 		}
+// 		else 
+// 		{
+// 			total = (10*11)+(90*10)+((distance-100)*9);
+// 		}
+// 		return total;
+
+
+function calculate(){
+    let distance = parseFloat(document.getElementById("km").value);
+    if(!distance){
+        document.getElementById("error").innerText = "Please Enter The Distance";
+    }
+    else if(distance<=0){
+        document.getElementById("error").innerText = "Please Enter Positive Distance";
+    }else{
+        document.getElementById("error").innerText = "";
+        let total = 0o0;
+        let sl1 = 0o0;
+        let sl2 = 0o0;
+        let sl3 = 0o0;
+        let ch1 = 0o0;
+        let ch2 = 0o0; 
+        let ch3 = 0o0;
+
+
+        if(distance <=10){
+            
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
