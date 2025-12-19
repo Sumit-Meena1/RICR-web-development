f8b3dbfabc413401db6496cd85c0ce1129fff22a
Sumit-Meena1 <sumitmeena6944@gmail.com>
2025-12-19 15:18:16 +0530

    attendanceTask

--- attendanceTask/script.js
+++ attendanceTask/script.js
@@ -1,27 +1,33 @@
-
 document.getElementById("a").disabled = true;
 document.getElementById("p").disabled = true;
-
-
-
-function submit(){
-    document.getElementById("a").style.backgroundColor = "red";
-     document.getElementById("p").style.backgroundColor = "green";
-
+let name = document.getElementById("name").value.trim();
+function submit() {
+  document.getElementById("a").style.backgroundColor = "red";
+  document.getElementById("p").style.backgroundColor = "green";
 }
 
-
-function take(){
-document.getElementById("a").disabled = false;
-document.getElementById("p").disabled = false;
-
-
+function take() {
+  document.getElementById("a").disabled = false;
+  document.getElementById("p").disabled = false;
+  window.location.href = "index.html";
 }
 
-
-function add(){
-    window.location.href = "add.html";
+function add() {
+  window.location.href = "add.html";
+  
+//   if (!name) {
+//     document.getElementById("error").innerText = "Required";
+//   } else {
+//     alert("Student Added Successfully!!")
+//   }
 }
-function done(){
-    window.location.href = "index.html";
+done(name);
+
+function done(name) {
+//   window.location.href = "index.html";
+  if (!name) {
+    document.getElementById("error").innerText = "Required";
+  } else {
+    alert("Student Added Successfully!!")
+  }
 }
