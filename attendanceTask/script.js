e61417f2cf76a3d18ab352812f7715b7f7d8713b
Sumit-Meena1 <sumitmeena6944@gmail.com>
2025-12-19 11:15:36 +0530

    attendanceTask

--- attendanceTask/script.js
+++ attendanceTask/script.js
@@ -1,6 +1,20 @@
+
+document.getElementById("a").disabled = true;
+document.getElementById("p").disabled = true;
+
+
+
 function submit(){
     document.getElementById("a").style.backgroundColor = "red";
      document.getElementById("p").style.backgroundColor = "green";
+
+}
+
+
+function take(){
+document.getElementById("a").disabled = false;
+document.getElementById("p").disabled = false;
+
 
 }
 
