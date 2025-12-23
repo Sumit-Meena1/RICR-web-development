c1fbdab6ebc39000de538191ba553d2d668b95eb
Sumit-Meena1 <sumitmeena6944@gmail.com>
2025-12-23 20:15:50 +0530

    fakeStore

--- fakeStore/product2.js
+++ fakeStore/product2.js
@@ -8,14 +8,14 @@ async function getProducts() {
             const d = document.createElement("div");
             d.classList.add( "p-3");
             d.innerHTML= `
-             <div class="chight border border-dark rounded shadow p-3 w-100 d-flex">
+             <div class="chight border border-dark border-opacity-50 rounded shadow p-3 w-100 d-flex">
             <div class="w-25  rounded-2"><img src=${element.image} alt=${element.title} class="w-100 h-100 object-fit-contain rounded-2"></div>
             <div class="w-75  p-2">
                 <div class="fw-bold fs-4">${element.title}</div>
                 <div class="fw-semibold">${element.rating.rate}/5 (${element.rating.count})</div>
                 <div class="fw-bold fs-5">${element.price*100}</div>
                 <div class="mb-2">${element.description}</div>
-                <div class="d-flex justify-content-center gap-3">
+                <div class="d-flex justify-content-start gap-3">
                     <button class="btn btn-outline-primary">Add tO Cart</button>
                     <button class="btn btn-primary">Buy Now</button>
                 </div>
