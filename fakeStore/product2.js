1d4f425a7a124c9858c159b7b051c0a2cdfc3e18
Sumit-Meena1 <sumitmeena6944@gmail.com>
2025-12-23 20:02:15 +0530

    fakeStore

--- fakeStore/product2.js
+++ fakeStore/product2.js
@@ -2,27 +2,23 @@ async function getProducts() {
     try{
         const res = await fetch("https://fakestoreapi.com/products");
         const data = await res.json();
-        const productList = document.getElementById("pdrow");
+        const productList = document.getElementById("pdcol");
 
         data.forEach(element => {
             const d = document.createElement("div");
-            d.classList.add("col-4", "p-3");
+            d.classList.add( "p-3");
             d.innerHTML= `
-             <div class="col-4 p-2">
-            <div class="card border  rounded shadow p-3">
-              <div class="h-50 border border-primary rounded-2">
-                <img src=${element.image} alt=${element.title} class="w-100 h-100 object-fit-contain rounded-2">
-              </div>
-              <div class="h-50 border p-2">
-                <div class="fw-bold fs-4">${element.title.length>50?element.title.slice(0, 45)+"...":element.title}</div>
+             <div class="chight border border-dark rounded shadow p-3 w-100 d-flex">
+            <div class="w-25  rounded-2"><img src=${element.image} alt=${element.title} class="w-100 h-100 object-fit-contain rounded-2"></div>
+            <div class="w-75  p-2">
+                <div class="fw-bold fs-4">${element.title}</div>
                 <div class="fw-semibold">${element.rating.rate}/5 (${element.rating.count})</div>
                 <div class="fw-bold fs-5">${element.price*100}</div>
-                <div class="mb-2">${element.description.slice(0,50)}...</div>
+                <div class="mb-2">${element.description}</div>
                 <div class="d-flex justify-content-center gap-3">
                     <button class="btn btn-outline-primary">Add tO Cart</button>
                     <button class="btn btn-primary">Buy Now</button>
                 </div>
-              </div>
             </div>
           </div>
             
@@ -30,7 +26,7 @@ async function getProducts() {
             `;
             productList.appendChild(d);
         });
-    }catch{
+    }catch(error){
         console.log(error.msg);
         
     }
