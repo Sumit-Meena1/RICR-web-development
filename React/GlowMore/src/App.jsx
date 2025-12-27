7be8594b771cfeba03759a232c8847310a78716f
Sumit-Meena1 <sumitmeena6944@gmail.com>
2025-12-27 19:43:02 +0530

    MakeUp

--- React/GlowMore/src/App.jsx
+++ React/GlowMore/src/App.jsx
@@ -1,1 +1,13 @@
+import React from "react";
 
+const App = () => {
+  return (
+    <>
+
+      <div className="p-3.5  m-['3.3']"></div>
+      <div className="bg-red-500" >App</div>
+    </>
+  );
+};
+
+export default App;
