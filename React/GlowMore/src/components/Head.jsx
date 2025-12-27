cb0a7e937d002ae456ea39397f3c37651566057c
Sumit-Meena1 <sumitmeena6944@gmail.com>
2025-12-27 20:20:36 +0530

    MakeUp

--- React/GlowMore/src/components/Head.jsx
+++ React/GlowMore/src/components/Head.jsx
@@ -4,11 +4,17 @@ import { Link } from "react-router-dom";
 const Head = () => {
   return (
     <>
-      <div>Glow More</div>
-      <div className="flex gap-3 bg-blue-700">
-            <Link to={"/"} className="text-decoration-none text-light">Home</Link>
-            <Link to={"/about"} className="text-decoration-none text-light">About</Link>
+      <div className="flex gap-3 bg-blue-700 justify-between">
+        <div className="text-white text-shadow-black">Glow More</div>
+        <div className="flex gap-4.5 mx-5">
+          <Link to={"/"} className="text-decoration-none text-light ">
+            Home
+          </Link>
+          <Link to={"/about"} className="text-decoration-none text-light">
+            About
+          </Link>
         </div>
+      </div>
     </>
   );
 };
