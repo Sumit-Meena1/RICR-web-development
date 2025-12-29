d7efd98d8267cf2dc6b2ff516b1c29c6302a396c
Sumit-Meena1 <sumitmeena6944@gmail.com>
2025-12-29 18:35:25 +0530

    GlowMore

--- React/GlowMore/src/pages/Home.jsx
+++ React/GlowMore/src/pages/Home.jsx
@@ -4,17 +4,17 @@ const Home = ()=> {
 
     return(
         <>
-        <div className=" text-black justify-center h-96 flex gap-16 py-24">
-            <div className="w-96 h-">
+        <div className=" text-black justify-center h-96 flex gap-60 py-24">
+            <div className="w-96">
 
-                <h1 className="text-6xl text-black ">Beauty & Makeup</h1>
+                <h1 className="text-6xl text-black my-10 ">Beauty & Makeup</h1>
 
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis fugiat harum in numquam quaerat aliquam. Velit voluptates quos ullam, tenetur dolorum adipisci quas quam libero sit sint incidunt, rem repudiandae iusto nemo numquam illum quae nisi praesentium voluptate, eveniet officiis harum voluptas. Error delectus nihil mollitia nostrum recusandae nisi quibusdam necessitatibus? Accusamus facilis vel quaerat dicta quod quia modi voluptatibus, soluta quis eligendi incidunt dolore voluptatum aliquam minus ipsum 
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis fugiat harum in numquam quaerat aliquam. Velit voluptates quos ullam, tenetur dolorum adipisci quas quam libero sit sint incidunt, rem repudiandae iusto nemo numquam illum quae nisi praesentium voluptate <br />
 
-                <button className="w-40 h-10 rounded-2xl border border-blue-900 bg-blue-500 hover:bg-blue-900 hover:text-white">Get Your Glow</button>
+                <button className="w-40 h-10 my-5 rounded-2xl border border-blue-900 bg-blue-500 hover:bg-blue-900 hover:text-white">Get Your Glow</button>
             </div>
             <div>
-                <img className="rounded border border-0.5 border-black" src="https://images01.nicepagecdn.com/page/45/94/website-template-preview-45949.jpg" alt="" />
+                <img className="rounded shadow" src="https://images01.nicepagecdn.com/page/45/94/website-template-preview-45949.jpg" alt="" width={600} />
             </div>
 
         </div>
