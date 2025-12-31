0616fb2d8b54f0f26082a1000a207dbcaec30759
Sumit-Meena1 <sumitmeena6944@gmail.com>
2025-12-31 12:04:33 +0530

    Coaching

--- React/Coaching/src/components/Header.jsx
+++ React/Coaching/src/components/Header.jsx
@@ -4,12 +4,9 @@ const Header = () => {
   return (
     <>
       <div className="d-flex bg-primary p-2 text-light align-items-center justify-content-between">
-        <h3>My MackUp</h3>
+        <h3>Coaching Registration</h3>
         <div className="d-flex gap-3">
             <Link to={"/"} className="text-decoration-none text-light">Home</Link>
-            <Link to={"/about"} className="text-decoration-none text-light">About</Link>
-            <Link to={"/product"} className="text-decoration-none text-light">Products</Link>
-            <Link to={"/contact"} className="text-decoration-none text-light">Contacts</Link>
             <Link to={"/login"} className="text-decoration-none text-light">LogIn</Link>
             <Link to={"/signup"} className="text-decoration-none text-light">SignUp</Link>
         </div>
