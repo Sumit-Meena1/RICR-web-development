0a7d94e2900400d0e721de104353d61b430b7112
Sumit-Meena1 <sumitmeena6944@gmail.com>
2026-01-07 17:01:58 +0530

    firstBackend

--- Node/firstBackend/index.js
+++ Node/firstBackend/index.js
@@ -5,12 +5,11 @@ import express from "express";
 const app = express();
 app.get("/", (req, res) => {
   console.log("server is running");
+  res.json({ message: "server is running susseccfully" });
 });
-
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=>{
-    console.log("server started at port ", port);
-    
-})
+app.listen(port, () => {
+  console.log("server started at port ", port);
+});
