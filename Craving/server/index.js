f9b7e0505dfa330e5f6b1689966c4251614e7990
Sumit-Meena1 <sumitmeena6944@gmail.com>
2026-01-12 22:55:48 +0530

    Project

--- Craving/server/index.js
+++ Craving/server/index.js
@@ -2,18 +2,28 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-import cors from 'cors';
+import cors from "cors";
+import morgan from "morgan";
 import connectDB from "./src/config/db.js";
+import AuthRouter from "./src/routers/authRouter.js";
 
 const app = express();
-app.use(cors({origin:"http:localhost:5173"}));
+app.use(cors({ origin: "http://localhost:5173" }));
 
 app.use(express.json());
+app.use(morgan("dev"));
 
-// app.use("/auth", AuthRouter);
+app.use("/auth", AuthRouter);
 
 app.get("/", (req, res) => {
   console.log("server is working");
+});
+
+app.use((err, req, res, next) => {
+  const ErrorMessage = err.message || "Internal Servar Error";
+  const StatusCode = err.StatusCode || 500;
+
+  res.status(StatusCode).json({ message: ErrorMessage });
 });
 
 const port = process.env.PORT || 5000;
---
+++ Craving/server/src/controllers/authController.js
@@ -0,0 +1,69 @@
+import User from "../models/userModel.js";
+import bcrypt from "bcrypt";
+
+export const UserRegister = async (req, res, next) => {
+  try {
+    const { fullName, email, mobileNumber, password } = req.body;
+    if (!fullName || !email || !mobileNumber || !password) {
+      const error = new Error("All Fields Required");
+      error.statusCode = 400;
+      return next(error);
+    }
+
+    const existingUser = await User.findOne({email});
+    if (existingUser) {
+      const error = new Error("Email Already Existed");
+      error.statusCode = 409;
+      return next(error);
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({
+      fullName,
+      email,
+      mobileNumber,
+      password: hashPassword,
+    });
+
+    console.log(newUser);
+    res.status(201).json({ message: "Registration Susseccfull" });
+  } catch (error) {
+    next(error);
+  }
+};
+export const UserLogin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      const error = new Error("All Fields Required");
+      error.statusCode = 400;
+      return next(error);
+    }
+
+    const existingUser = await User.findOne({email});
+    if (!existingUser) {
+      const error = new Error("Email Not Registered");
+      error.statusCode = 402;
+      return next(error);
+    }
+
+    const isVerified = await bcrypt.compare(password, existingUser.password);
+    if (!isVerified) {
+      const error = new Error("Password didn't Match");
+      error.statusCode = 402;
+      return next(error);
+    }
+
+    res.status(200).json({ message: "Login Successfull", data: existingUser });
+  } catch (error) {
+    next(error);
+  }
+};
+export const UserLogout = async (req, res, next) => {
+  try {
+    res.status(200).json({ message: "Logout Successfull" });
+  } catch (error) {
+    next(error);
+  }
+};
