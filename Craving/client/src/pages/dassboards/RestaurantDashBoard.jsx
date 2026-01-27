28d59d6dfa4aafb4d7563d1f17c672a5ff93e9ba
Sumit-Meena1 <sumitmeena6944@gmail.com>
2026-01-27 18:12:11 +0530

    project

--- Craving/client/src/pages/dassboards/RestaurantDashBoard.jsx
+++ Craving/client/src/pages/dassboards/RestaurantDashBoard.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const RestaurantDashBoard = () => {
+  
   return (
+    <>
     <div>RestaurantDashBoard</div>
+
+    </>
   )
+
 }
 
 export default RestaurantDashBoard
--- Craving/client/src/pages/dassboards/RiderDashBoard.jsx
+++ Craving/client/src/pages/dassboards/RiderDashBoard.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const RiderDashBoard = () => {
   return (
-    <div>RiderDashBoard</div>
+    <>
+    <div>
+        
+    </div>
+    </>
   )
 }
 
